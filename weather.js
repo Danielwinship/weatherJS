e712855144f255f8433cd6e5d2dd4742f7f1a44e
Daniel Winship <danielwinship@icloud.com>
2019-03-26 06:43:29 -0400

    changed state to country

--- weather.js
+++ weather.js
@@ -1,25 +1,25 @@
-https://api.openweathermap.org/data/2.5/weather?q=London,uk&appid=de6afde2fb56fdb27c2e3e7f78c57626
-
+//https://api.openweathermap.org/data/2.5/weather?q=London,uk&appid=de6afde2fb56fdb27c2e3e7f78c57626
+// https://openweathermap.org/img/w/02d.png
 class Weather {
-  constructor(city, state) {
+  constructor(city, country) {
     this.apiKey = 'de6afde2fb56fdb27c2e3e7f78c57626';
     this.city = city;
-    this.state = state;
+    this.country = country;
   }
 
 // fetch weather from api
 async getWeather() {
-  const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${this.city},${this.state}&appid=${this.apiKey}`)
+  const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${this.city},${this.country}&units=imperial&appid=${this.apiKey}`)
 
   const responseData = await response.json();
 
-  return responseData.weather;
+  return responseData;
 }
 
 // Change weather location
-changeLocation(city, state) {
+changeLocation(city, country) {
   this.city = city;
-  this.state = state;
+  this.country = country;
 }
 
 }
