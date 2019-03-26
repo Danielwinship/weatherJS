d35a6b9d4e9850c533878e8b92ff67f95d22212e
Daniel Winship <danielwinship@icloud.com>
2019-03-26 18:11:42 -0400

    created UI and called paint func

--- app.js
+++ app.js
@@ -1,13 +1,20 @@
 // init weather object
 const weather = new Weather('Tampa', 'us');
 
+
+// Init UI
+const ui = new UI();
+
 // get weather on DOM load
 document.addEventListener('DOMContentLoaded', getweather);
+
+
 
 function getweather(){
   weather.getWeather()
   .then(results => {
     console.log(results);
+    ui.paint(results);
   })
   .catch(err => console.log(err));
 }
