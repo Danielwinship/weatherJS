54326d730d684d2e9f24fc8e2b32c45077c796b5
Daniel Winship <danielwinship@icloud.com>
2019-03-26 20:34:20 -0400

    added change location button

--- app.js
+++ app.js
@@ -1,5 +1,10 @@
+// init storage
+const storage = new Storage();
+// get stored location data
+  const weatherLocation = storage.getLocationData();
+
 // init weather object
-const weather = new Weather('Tampa', 'us');
+const weather = new Weather(weatherLocation.city, weatherLocation.country);
 
 
 // Init UI
@@ -7,6 +12,25 @@ const ui = new UI();
 
 // get weather on DOM load
 document.addEventListener('DOMContentLoaded', getweather);
+
+
+// Change location event
+document.getElementById('w-change-btn').addEventListener('click', () => {
+  const city = document.getElementById('city').value;
+  const country = document.getElementById('country').value;
+
+  weather.changeLocation(city, country);
+
+  // Get and display weather
+  getweather();
+
+  // set location
+  storage.setLocationData(city,country);
+  
+
+  // Close modal
+  $('#locModal').modal('hide');
+});
 
 
 
